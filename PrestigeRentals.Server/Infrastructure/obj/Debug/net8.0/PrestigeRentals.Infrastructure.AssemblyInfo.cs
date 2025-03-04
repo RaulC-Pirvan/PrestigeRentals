6302e197c0f7a9da35b210f6c07705b0342f97d9
Raul-Costin Pîrvan <150127119+RaulC-Pirvan@users.noreply.github.com>
2025-03-04 18:15:57 +0200

    Initiate on second device

--- PrestigeRentals.Server/Infrastructure/obj/Debug/net8.0/PrestigeRentals.Infrastructure.AssemblyInfo.cs
+++ PrestigeRentals.Server/Infrastructure/obj/Debug/net8.0/PrestigeRentals.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrestigeRentals.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce03a38fb7da4e14911b80a4c35f05040e07d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dde3ea8142e93cdfd0197f203107ccb9f134084")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrestigeRentals.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrestigeRentals.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
