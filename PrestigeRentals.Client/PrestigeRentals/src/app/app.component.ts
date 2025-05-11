d81a8e6890cee56c51a5ca80cfce19f05d5ca3a1
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-11 15:54:01 +0300

    Create footer

--- PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
