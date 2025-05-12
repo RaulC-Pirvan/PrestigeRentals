52385a6194bc60da75a864343c3b79e4a9626853
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-12 16:37:12 +0300

    Create 404 Page

--- PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPageComponent],
+  imports: [LandingPageComponent, NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
