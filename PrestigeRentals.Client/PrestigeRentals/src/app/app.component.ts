89495e2acb74eef9f3614a7038f33148079ef5d8
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-12 16:39:47 +0300

    Add 403 Page

--- PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ForbiddenAccessComponent } from './pages/forbidden-access/forbidden-access.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPageComponent, NotFoundComponent],
+  imports: [ForbiddenAccessComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ PrestigeRentals.Client/PrestigeRentals/src/app/pages/forbidden-access/forbidden-access.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/button/button.component';
+
+@Component({
+  selector: 'app-forbidden-access',
+  imports: [ButtonComponent],
+  templateUrl: './forbidden-access.component.html',
+  styleUrl: './forbidden-access.component.scss'
+})
+export class ForbiddenAccessComponent {
+
+}
