caf1bd8364419dca739a60225da74da3e3ad4e52
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-17 14:50:11 +0300

    Add notification toast

--- PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
@@ -9,15 +9,24 @@ import {
 } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { NotificationComponent } from '../notification/notification.component';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ButtonComponent, TitleComponent, ReactiveFormsModule],
+  imports: [
+    ButtonComponent,
+    TitleComponent,
+    ReactiveFormsModule,
+    NotificationComponent,
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
   loginForm: FormGroup;
+
+  notificationMessage: string | null = null;
+  notificationType: 'success' | 'error' = 'success';
 
   constructor(
     private fb: FormBuilder,
@@ -31,25 +40,39 @@ export class LoginFormComponent {
     });
   }
 
+  showNotification(message: string, type: 'success' | 'error') {
+    this.notificationMessage = message;
+    this.notificationType = type;
+
+    setTimeout(() => {
+      this.notificationMessage = null;
+    }, 3000); 
+  }
+
   onSubmit(): void {
     if (this.loginForm.valid) {
       const { email, password, rememberMe } = this.loginForm.value;
 
-      this.http.post<{token: string}>('/api/login', { email, password }).subscribe({
-        next: (response) => {
-          const token = response.token;
-          if (rememberMe) {
-            localStorage.setItem('authToken', token);
-          } else {
-            sessionStorage.setItem('authToken', token);
-          }
-          this.router.navigate(['/']);
-        },
-        error: (error) => {
-          console.error('Login failed', error);
-          alert('Invalid credentials');
-        },
-      });
+      this.http
+        .post<{ token: string }>('/api/login', { email, password })
+        .subscribe({
+          next: (response) => {
+            const token = response.token;
+            if (rememberMe) {
+              localStorage.setItem('authToken', token);
+            } else {
+              sessionStorage.setItem('authToken', token);
+            }
+            this.showNotification('Login successful!', 'success');
+            setTimeout(() => {
+              this.router.navigate(['/']);
+            }, 1000);
+          },
+          error: (error) => {
+            this.showNotification('Invalid credentials', 'error');
+            console.error('Login failed', error);
+          },
+        });
     }
   }
 }
