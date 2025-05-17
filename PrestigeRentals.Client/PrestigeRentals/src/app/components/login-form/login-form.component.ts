45aeb73fa3de58497d4cab0662a2e09a6783b90f
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-17 14:27:04 +0300

    Add Login Functionality

--- PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { TitleComponent } from '../../shared/title/title.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ButtonComponent, TitleComponent],
+  imports: [ButtonComponent, TitleComponent, ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password, rememberMe } = this.loginForm.value;
+
+      this.http.post<{token: string}>('/api/login', { email, password }).subscribe({
+        next: (response) => {
+          const token = response.token;
+          if (rememberMe) {
+            localStorage.setItem('authToken', token);
+          } else {
+            sessionStorage.setItem('authToken', token);
+          }
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+          alert('Invalid credentials');
+        },
+      });
+    }
+  }
 }
