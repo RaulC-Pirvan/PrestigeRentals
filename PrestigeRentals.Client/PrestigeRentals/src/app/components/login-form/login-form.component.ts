fc5fa110e572765b1e66a9f0e1041a541063c5f3
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-17 13:50:49 +0300

    Fix Login Page

--- PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/components/login-form/login-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { TitleComponent } from '../../shared/title/title.component';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, TitleComponent],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
