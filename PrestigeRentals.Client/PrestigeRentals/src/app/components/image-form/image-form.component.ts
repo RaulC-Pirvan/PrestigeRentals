66fdf25c32c029a453cfcf0a55afa3de764fd7b3
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-05-21 12:38:22 +0300

    Link contact form to backend

--- PrestigeRentals.Client/PrestigeRentals/src/app/components/image-form/image-form.component.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/components/image-form/image-form.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { TitleComponent } from '../../shared/title/title.component';
+import { TicketService } from '../../services/ticket.service';
+import { CreateTicketRequest } from '../../models/create-ticket-request.model';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-image-form',
-  imports: [ButtonComponent, TitleComponent],
+  standalone: true,
+  imports: [ButtonComponent, TitleComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './image-form.component.html',
   styleUrl: './image-form.component.scss'
 })
 export class ImageFormComponent {
+  request: CreateTicketRequest = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    description: ''
+  };
 
+  constructor(private ticketService: TicketService, private notificationService: NotificationService) {}
+
+  onSubmit() {
+    this.ticketService.submitTicket(this.request).subscribe({
+      next: (res) => {
+        this.notificationService.show('Ticket submitted successfully!', 'success');
+      },
+      error: (err) => {
+        this.notificationService.show('Failed to submit ticket.', 'error');
+      }
+    });
+  }
 }
