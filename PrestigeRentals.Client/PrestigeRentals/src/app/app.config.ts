71294dbc5e215e26a3b5c38d1ceae27e01a49873
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-06-01 13:30:16 +0300

    Fix server again and stylize admin navbar

--- PrestigeRentals.Client/PrestigeRentals/src/app/app.config.ts
+++ PrestigeRentals.Client/PrestigeRentals/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 import {
@@ -15,6 +18,12 @@ import { AuthInterceptor } from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
