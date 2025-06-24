bb574917f82e2e0f9291d18465568618b922f580
Raul-Costin Pîrvan <Raul.Pirvan76@gmail.com>
2025-06-24 15:37:10 +0300

    Add XML docs. to workers

--- PrestigeRentals.Server/Application/Workers/OrderBackgroundWorker.cs
+++ PrestigeRentals.Server/Application/Workers/OrderBackgroundWorker.cs
@@ -2,25 +2,35 @@
 using Microsoft.Extensions.Hosting;
 using PrestigeRentals.Application.Services.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PrestigeRentals.Infrastructure.Workers
 {
+    /// <summary>
+    /// A background worker that periodically checks and updates expired vehicle rental orders.
+    /// </summary>
     public class OrderBackgroundWorker : BackgroundService
     {
         private readonly IServiceProvider _serviceProvider;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderBackgroundWorker"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The application's service provider for scoped dependency resolution.</param>
         public OrderBackgroundWorker(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Periodically executes the order expiration service in a background loop.
+        /// </summary>
+        /// <param name="stoppingToken">Token used to cancel the background task.</param>
+        /// <returns>A task that represents the background execution operation.</returns>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while(!stoppingToken.IsCancellationRequested)
+            while (!stoppingToken.IsCancellationRequested)
             {
                 using var scope = _serviceProvider.CreateScope();
                 var service = scope.ServiceProvider.GetRequiredService<IOrderExpirationService>();
